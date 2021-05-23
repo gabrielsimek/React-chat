f7e69b2006cc6907d7d71a58eb6dd676810e5625
Gabriel Simek <gabrielsimek@outlook.com>
2021-05-22 21:22:49 -0700

    basic react chat app

--- src/app/App.js
+++ src/app/App.js
@@ -1,53 +1,63 @@
 import { Component } from 'react';
-import Header from './Header';
-import Footer from './Footer';
-import Home from '../home/Home';
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-  Redirect
-} from 'react-router-dom';
 import './App.css';
 
-class App extends Component {
+import { io } from 'socket.io-client';
 
-  render() {
+
+export default class Chat extends Component {
+  state = {
+    chat: '',
+    socket: {},
+    message: ''
+  }
+
+  componentDidMount(){
+    const socket = io('http://localhost:3000/', {
+      extraHeaders: {
+        'my-custom-header': 'abcd'
+      }
+    });
+    this.setState({ socket: socket });
+  
+    socket.on('chat message', (message) => {
+      this.handleMessage(message);
+    });
+  }
+  handleSubmit = e => {
+    const { socket } = this.state;
+    e.preventDefault();
+    const { chat } = this.state;
+    
+    socket.emit('chat message', chat);
+    
+  }
+
+  handleMessage = (newMessage) => {
+
+    this.setState({ message: newMessage });
+
+  }
+  handleChat = e => {
+    this.setState({ chat: e.target.value });
+  }
+
+  render(){
+    const { chat, message } = this.state;
+
+   
+    
     return (
-      <div className="App">
-        <Router>
-          <Header/>
-          <main>
-
-            <Switch>
-              <Route path="/" exact={true}
-                render={routerProps => (
-                  <Home {...routerProps}/>
-                )}
-              />
-
-              <Route path="/resources" exact={true}
-                render={routerProps => (
-                  <div>Implement a page of resources</div>
-                )}
-              />
-
-              <Route path="/resources/:id"
-                render={routerProps => (
-                  <div>Implement a page for id {routerProps.match.params.id}</div>
-                )}
-              />
-
-              <Redirect to="/" />
-
-            </Switch>
-          </main>
-          <Footer/>
-        </Router>
+     
+      <div className="chat">
+        <ul>
+          <li id="messages"></li>
+        </ul>
+        <form onSubmit={this.handleSubmit}id="form" action="">
+          <input value={chat} onChange={this.handleChat} id="input" /><button>Send</button>
+        </form>
+        <p>{message}</p>
       </div>
     );
   }
 
 }
-
-export default App;
